4d75f2a1bc6481d715d2e3a20e8f5eb94530d0df
Nicholas La Claire <nlaclaire@yahoo.com>
2023-01-27 16:52:39 -0500

    Fixed incorrect declarations.

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
+
+
 // 1) Define a variable named myNumber.
-var myNumber; 
+let myNumber; 
 
 // 2) Assign a value of 5 to the variable myNumber.
-var myNumber = 5;
+myNumber = 5;
 
 // 3) Define a variable named myOtherNumber and assign it a value of 10
-var myOtherNumber = 10;
+let myOtherNumber = 10;
 
 /* 4) Define a variable named myTotal and
 assign the product of myNumber and myOtherNumber to it */
-var myTotal = (mynumber * myOtherNumber);
+let myTotal = mynumber * myOtherNumber;
 
 /* 5) Define a variable named myName and assign your name as the value.
 Define a 2nd variable named myCombo and assign it the result of the variables
 myName + myNumber */
-var myName = "Nicholas La Claire.";
+let myName = "Nicholas La Claire.";
 
-var myCombo = (myName + myNumber);
+let myCombo = myName + myNumber;
 
 /* 6) Define a variable named myOtherCombo. Set the value of myOtherCombo equal to
 the variable myNumber + 8 */
-var myOtherCombo = (myNumber + 8);
+let myOtherCombo = myNumber + 8;
 
 // 7) Assign the remainder of myNumber divided by 5 to a variable named myRemainder
-var myRemainder = (myNumber%5);
+let myRemainder = myNumber%5;
 
 /* 8) Assign a value of 1 to a variable named i.
 On the next line, increment the value of i with a postfix operator */
-var i = 1;
+let i = 1;
 i++;
 
 /* 9) Assign a value of 5 to a variable named int.
 On the following line, decrement int using a prefix operator */
-var int = 5; 
+let int = 5; 
 --int; 
 
 // 10) Assign an equality check of (myNumber === myName) to a variable named myBoolean
-myBoolean = (myNumber === myName);
+let myBoolean = myNumber === myName;
